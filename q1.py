75e5faddd5d9307b4c4120d778d62b98c43e155c
shaswotdhakal <158523981+shaswotdhakal@users.noreply.github.com>
2024-02-14 19:36:23 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,18 @@
+def isPrime(num):
+"""
+This function check if the number is prime.
+"""
+
+if num < 2: 
+   return False
+for i in range(2, int(num**0.5)+1):
+   if num % i == 0:
+     return False
+return True
+
 def getPrimeNumbers(n):
+  """
+  THis function returns a list of all prime numbers between 2 and n.
+  """
+
+return [num for num in range(2, n+1) if isPrime(num)]
